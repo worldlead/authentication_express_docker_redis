88efe5e3b8306b71dea81a32e4a6be7bd050cd88
Cosep Linux <procosep@gmail.com>
2024-02-27 23:31:57 -0800

    feat: added authentication middleware and controller

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express"
+import { findAllUsers } from "../services/user.service"
+
+export const getMeHandler = (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const user = res.locals.user
+        res.status(200).json({
+            status: 'success',
+            data: {
+                user,
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getAllUserHandler = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const users = await findAllUsers()
+        res.status(200).json({
+            status: 'success',
+            result: users.length,
+            data: {
+                users,
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+}
